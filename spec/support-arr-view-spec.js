67893bf39c97d13156a095f0f98113a20ede3569
phoebs88 <42778263+phoebs88@users.noreply.github.com>
2019-03-11 06:53:24 +0530

    Add files via upload

---
+++ spec/support-arr-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SupportArrView from '../lib/support-arr-view';
+
+describe('SupportArrView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
